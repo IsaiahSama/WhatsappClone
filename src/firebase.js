8ee79cf8d215a1e67a6f627fb0bbba548a9de16d
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-12 17:03:02 -0400

    Setup firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBZBD-OM1q6BkVk-34EBCoPat5iWgdmkXA",
@@ -8,3 +12,11 @@ const firebaseConfig = {
   appId: "1:526226771983:web:e4f9d156c5e9b094de937a",
   measurementId: "G-LXWNKQ31C4",
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, db };
+export default app;
