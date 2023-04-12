d12ad9bfc8604212ba7f3ee63c4067bdeefb7010
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-12 15:49:20 -0400

    Added Sidebar componenet and styled them

---
+++ src/Sidebar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar, IconButton } from "@mui/material";
+import {
+  Chat,
+  DonutLarge,
+  MoreVert,
+  SearchOutlined,
+} from "@mui/icons-material";
+import SidebarChat from "./SidebarChat";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" />
+        </div>
+      </div>
+
+      <div className="sidebar__chats">
+        {/* Sidebar Chats can go here */}
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
